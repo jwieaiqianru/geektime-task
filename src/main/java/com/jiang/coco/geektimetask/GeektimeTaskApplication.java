b09e858b92f28f2efdd393c49ae01e7d03f31a6b
luosa <luosa@byai.com>
2022-03-12 10:07:25 +0800

    first commit

---
+++ src/main/java/com/jiang/coco/geektimetask/GeektimeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.jiang.coco.geektimetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeektimeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeektimeTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiang/coco/geektimetask/GeektimeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.coco.geektimetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeektimeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
